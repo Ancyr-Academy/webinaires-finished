99d51d719a965ec114eaee9447b5e06904f65498
Anthony Cyrille <ancyr@pm.me>
2023-09-15 18:52:12 +0200

    refactor: gateways with real impls

--- src/adapters/nest/webinaires/gateways.ts
+++ src/adapters/nest/webinaires/gateways.ts
@@ -1,11 +1,15 @@
+import { getModelToken } from '@nestjs/mongoose';
+
 import { I_WEBINAIRE_QUERY } from '../../../modules/webinaires/read/ports/webinaire.query';
 import { InMemoryWebinaireQuery } from '../../../modules/webinaires/read/adapters/in-memory-webinaire-query';
 import { I_WEBINAIRE_REPOSITORY } from '../../../modules/webinaires/write/ports/webinaire.repository';
-import { InMemoryWebinaireRepository } from '../../../modules/webinaires/write/adapters/in-memory-webinaire-repository';
 import { I_PARTICIPANT_QUERY } from '../../../modules/webinaires/read/ports/participant.query';
 import { InMemoryParticipantQuery } from '../../../modules/webinaires/read/adapters/in-memory-participant-query';
 import { I_PARTICIPATION_REPOSITORY } from '../../../modules/webinaires/write/ports/participation.repository';
-import { InMemoryParticipationRepository } from '../../../modules/webinaires/write/adapters/in-memory-participation-repository';
+import { MongoWebinaire } from './models/mongo-webinaire';
+import { MongoWebinaireRepository } from './adapters/mongo.webinaire-repository';
+import { MongoParticipation } from './models/mongo-participation';
+import { MongoParticipationRepository } from './adapters/mongo.participation-repository';
 
 export const gateways = [
   {
@@ -16,8 +20,9 @@ export const gateways = [
   },
   {
     provide: I_WEBINAIRE_REPOSITORY,
-    useFactory: () => {
-      return new InMemoryWebinaireRepository();
+    inject: [getModelToken(MongoWebinaire.CollectionName)],
+    useFactory: (model) => {
+      return new MongoWebinaireRepository(model);
     },
   },
   {
@@ -28,8 +33,9 @@ export const gateways = [
   },
   {
     provide: I_PARTICIPATION_REPOSITORY,
-    useFactory: () => {
-      return new InMemoryParticipationRepository();
+    inject: [getModelToken(MongoParticipation.CollectionName)],
+    useFactory: (model) => {
+      return new MongoParticipationRepository(model);
     },
   },
 ];
